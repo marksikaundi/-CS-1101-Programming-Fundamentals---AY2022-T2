4c59798b429e37c1c21e93adcfa5773db3ffcd05
marksikaundi <emmanuelmark117@gmail.com>
2022-07-08 00:23:17 +0200

    learning journal questions

---
+++ Unit 3/Question 2.py
@@ -0,0 +1,20 @@
+# QUESTION 2 
+# 
+# my blood group
+myBloodGroup = 'AB'
+
+print(mybloodGroup)
+
+# Output demonstrating the runtime error, including the error message.
+
+# myBloodGroup = 'ABC'
+# print(mybloodGroup)
+#Traceback (most recent call last):
+# File "<stdin>", line 1, in <module>
+#NameError: name 'mybloodGroup' is not defined. Did you mean: 'myBloodGroup'?
+
+# An explanation of the error message.
+# the the intergrated development enviroment(IDE) detacted to say NameError, meaning the name i have entered in the print section is not correct or i havent defined it as seen in the  out of the code and it even show the suggestion to say did you mean myBloodGroup.
+
+# An explanation of how best the error an be fixed.
+# The solution to this question is that, all we need to do is by going to our print section and check the string we have entered if its correct with the one we have defined at first, then after its corrected then once that is done, we can run the codes and see if we get the results.

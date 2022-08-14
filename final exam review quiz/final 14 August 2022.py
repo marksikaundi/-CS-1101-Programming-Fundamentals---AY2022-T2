61e8930d3b1c33515d57eae94a08445ea3de1228
marksikaundi <emmanuelmark117@gmail.com>
2022-08-14 14:48:05 +0200

    final exam files added

---
+++ final exam review quiz/final 14 August 2022.py
@@ -0,0 +1,92 @@
+# # # # # # # # # # # x = 5
+# # # # # # # # # # # if x % 2 == 1:
+# # # # # # # # # # #     print (x)
+# # # # # # # # # # # else:
+# # # # # # # # # # #     print (x, x%2)
+
+
+
+
+
+
+
+
+# # # # # # # # # # index = "Ability is a poor man's wealth".find("w")
+# # # # # # # # # # print(index)
+
+# # # # # # # # # # def function2(param):
+# # # # # # # # # #     print (param, param)
+
+# # # # # # # # # # def function1(part1, part2):
+# # # # # # # # # #     cat = part1 + part2
+# # # # # # # # # #     function2(cat)
+
+# # # # # # # # # # chant1 = "See Me "
+# # # # # # # # # # chant2 = "See You "
+# # # # # # # # # # function1(chant1, chant2)
+
+
+
+# # # # # # # # # n = 10
+# # # # # # # # # while n != 1:
+# # # # # # # # #     print (n,)
+# # # # # # # # #     if n % 2 == 0: # n is even
+# # # # # # # # #         n = n // 2
+# # # # # # # # #     else: # n is odd
+# # # # # # # # #         n = n * 3 + 1
+
+
+
+# # # # # # # # # "bib".find('b', 1, 2)
+
+
+# # # # # # # # # pi = float(3.14159)
+# # # # # # # # # print (pi)
+
+
+# # # # # # # # def procedure( n ):
+# # # # # # # #     while n > 0:
+# # # # # # # #         print (n,)
+# # # # # # # #         n = n - 1
+
+# # # # # # # def recurse(a):
+# # # # # # #     if (a == 0):
+# # # # # # #         print(a)
+# # # # # # #     else:
+# # # # # # #         recurse(a) 
+
+# # # # # # # recurse(0)
+
+# # # # # # # if choice == 'a':
+# # # # # # #      draw_a()
+# # # # # # # elif choice == 'b' or choice == 'a':
+# # # # # # #      draw_b()
+
+
+# # # # # # def recurse():
+
+# # # # # #     recurse()
+
+# # # # # # recurse()
+
+
+
+# # # # # # #
+
+# # # # # print('%s %d' % (5, 'dollars'))
+
+# # # # print('%s %d' % (5, 'dollars'))
+
+# # # fin = open('words.txt')
+# # # for line in fin:
+# # #     word = line.strip()
+# # #     print(word)
+
+# # not(True or False)
+
+# n = 1000
+# count = 0
+# while n:
+#     count = count + 1
+#     n = n // 10
+# print (count)
---
+++ final exam review quiz/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+"bib".find('b', 1, 2)
+
+
+# pi = float(3.14159)
+# print (pi)

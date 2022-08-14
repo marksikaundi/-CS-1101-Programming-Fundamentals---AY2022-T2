463270c9708c9d5259a4250301b28cfced9bab3b
marksikaundi <emmanuelmark117@gmail.com>
2022-08-14 10:00:09 +0200

    added final quiz review

---
+++ final exam review quiz/testing.py
@@ -0,0 +1,121 @@
+# # # # # # # # # # print("hello, am doing my final exam")
+
+# # # # # # # # # # x = 5
+# # # # # # # # # # if x % 2 == 0:
+# # # # # # # # # #     print (x)
+# # # # # # # # # # else:
+# # # # # # # # # #     print (x, x%2)
+
+# # # # # # # # # # my_list = [3, 2, 1]
+# # # # # # # # # # print(my_list.sort())
+
+
+# # # # # # # # # # "bib".find('b', 1, 2)
+
+# # # # # # # # # # fin = open('words.txt')
+# # # # # # # # # # for line in fin:
+# # # # # # # # # #     word = line.strip()
+# # # # # # # # # #     print(word)
+
+
+# # # # # # # # # def area(l, w):
+# # # # # # # # #     temp = l * w;
+# # # # # # # # #     return temp
+
+# # # # # # # # # l = 4.0
+# # # # # # # # # w = 3.25
+# # # # # # # # # x = area(l, w)
+# # # # # # # # # if ( x ):
+# # # # # # # # #   print (x)
+
+
+# # # # # # # # #   index = "Ability is a poor man's wealth".find("W")
+# # # # # # # # # print(index)
+
+
+# # # # # # # # import string
+
+# # # # # # # # index = "Ability is a poor man's wealth".find("w")
+# # # # # # # # print(index)
+
+# # # # # # # # print ("%s %d %f" % (5, 5, 5))
+
+
+
+# # # # # # # # for k in d:
+# # # # # # # #     if d[k] == v:
+# # # # # # # #     return k
+
+# # # # # # # d.items()
+
+# # # # # # print('%d %s' % (5, 'dollars'))
+
+# # # # # for fruit in ["banana", "apple", "quince"]:
+# # # # #     print (fruit)
+
+# # # # #     pi = float(3.14159)
+# # # # # print (pi)
+
+
+# # # # # x=1
+# # # # # y=2
+# # # # # if x == y:
+# # # # #     print (x, "and", y, "are equal")
+# # # # # else:
+# # # # #     if x < y:
+# # # # #         print (x, "is less than", y)
+# # # # #     else:
+# # # # #         print (x, "is greater than", y)
+
+# # # # # fin = open('words.txt')
+# # # # # for line in fin:
+# # # # #     word = line.strip()
+# # # # #     print(word)
+
+# # # # mylist = [ [2,4,1], [1,2,3], [2,3,5] ]
+# # # # a=0
+# # # # b=0
+# # # # total = 0
+# # # # while a <= 2:
+# # # #     while b < 2:
+# # # #         total += mylist[a][b]
+# # # #         b += 1
+# # # #     a += 1
+# # # #     b = 0 
+# # # # print (total)
+
+# # # # not(True and False)
+
+
+
+# # # # result = dict()
+# # # # for key in d:
+# # # #     val = d[key]
+# # # #     if val not in inverse:
+# # # #         result[val] = [key]
+# # # #     else:
+# # # #         result[val].append(key)
+
+# # # fin = open('words.txt')
+# # # for line in fin:
+# # #     word = line.strip()
+# # #     print(word)
+
+# # def function2(param):
+# #     print (param, param)
+
+# # def function1(part1, part2):
+# #     cat = part1 + part2
+# #     function2(cat)
+
+# # chant1 = "See Me "
+# # chant2 = "See You "
+# # function1(chant1, chant2)
+
+# # type(0.123)
+
+# type(0.123)
+
+
+# mylist = [1, 4, 2, 3]
+# mylist.append(5)
